ecd207d2f416e373c8dba85e5a07658f6300424e
ShlomiTal <68659928+ShlomiTal@users.noreply.github.com>
2025-05-25 11:19:45 +0300

    Add files via upload

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export const config = {
     binance: {
-      apiKey: 'YOUR_BINANCE_API_KEY',
-      apiSecret: 'YOUR_BINANCE_API_SECRET',
+      apiKey: 'PLzDTYO7s4liw6y9B7rN7ZYzwZwUHfGggGe7pgmKNrLuaSLV9B9fSxpGxUoag4liY',
+      apiSecret: 'dUqDoSIbUKlOccNMjGfChvBqR3iZpcg8bGj1rAuuu7qgY0t8mINfYDl0q94eUIsA',
     },
   };
   
