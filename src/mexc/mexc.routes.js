20cea2c66569b5cecccdab9c94d949b584ec19f9
ShlomiTal <68659928+ShlomiTal@users.noreply.github.com>
2025-05-25 11:31:56 +0300

    Delete src/mexc directory

--- src/mexc/mexc.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAccountSummary } from './mexc.controller.js';
-
-const router = express.Router();
-
-// Define MEXC-related endpoints here
-router.get('/account-summary', getAccountSummary);
-
-export default router;
